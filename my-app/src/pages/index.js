0e1035dee6e5e2a33f85fae5e4c42939324e0700
Karina Machado <karinamachado@msn.com>
2018-11-05 23:01:46 -0300

    upload project

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+        <div className="HeroGroup">
+            <h1>Learn to design and code React apps</h1>
+            <p>Complete course about the best tools and design systems.Prototype and build apps with React and Swift.</p>
+            <link to="/video">Watch the video</link>
+        </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
